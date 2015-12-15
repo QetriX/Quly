69b87b16e64a4f2a571422da07f2d92dc295ce05
mikiqex <michal.novak@qex.cz>
2015-12-15 12:36:45 +0100

    Create Item.cs

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+namespace Quly
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	[Serializable]
+	public class Item
+	{
+		protected string _name;
+		protected float _weight = 1;
+		// List<Item> _rewards;
+
+		protected int _materialCapacity = 100; // How much material can the item contain (max)
+		protected int _materialCotnent = 0; // How much material the item contains
+		protected string _materialType; // What Material the item accepts or contains
+
+		public float weight()
+		{
+			return _weight;
+		}
+	}
+}
